0a2be74446d975dfab56190d696ed6d99281cca8
“gil-son” <gilszon@gmail.com>
2021-10-03 01:05:04 -0300

    Controller

--- bds02/src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ bds02/src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -1,9 +1,21 @@
 package com.devsuperior.bds02.controllers;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.bds01.dto.CityDTO;
+import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
+import com.devsuperior.dscatalog.dto.CityDTO;
+
 
 @RestController
 @RequestMapping(value = "/cities")
@@ -16,6 +28,18 @@ public class CityController {
 	CityController(CityService service) {
 		this.service = service;
 	}
+	
+	
+	
+	
+	@GetMapping
+	public ResponseEntity<List<CityDTO>> findAll(){
+		
+		List<CityDTO> listDTO = service.findAll();
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	
 	
 	
 }
