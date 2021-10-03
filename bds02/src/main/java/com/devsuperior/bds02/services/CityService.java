b46c7b5140f15bb274fba75a78950f359b72ee5b
“gil-son” <gilszon@gmail.com>
2021-10-03 01:05:26 -0300

    Service

--- bds02/src/main/java/com/devsuperior/bds02/services/CityService.java
+++ bds02/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,8 +1,17 @@
 package com.devsuperior.bds02.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.entities.City;
+
+import com.devsuperior.bds02.dto.CityDTO;
+
 
 @Service
 public class CityService {
@@ -13,5 +22,12 @@ public class CityService {
 	CityService(CityRepository repository){
 		this.repository = repository;
 	}
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> list = repository.findAll(Sort.by(Sort.Direction.ASC,"name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
 
 }
